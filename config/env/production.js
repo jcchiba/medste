339d4a5b7b05c2229df6b5b1a4812e38da1dda73
gordamed <gorda@the.ms>
2020-05-10 23:41:34 +0900

    qq

--- config/env/production.js
+++ config/env/production.js
@@ -250,10 +250,10 @@ module.exports = {
     * > Be sure to use the right protocol!  ("http://" vs. "https://")         *
     *                                                                          *
     ***************************************************************************/
-    // onlyAllowOrigins: [
-    //   'https://example.com',
+     onlyAllowOrigins: [
+       'https://hellomed.herokuapp.com',
     //   'https://staging.example.com',
-    // ],
+     ],
 
 
     /***************************************************************************
